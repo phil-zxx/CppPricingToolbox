3c7f9b18392e0bea626447009022b3b333e5ce57
phil-zxx <32247590+phil-zxx@users.noreply.github.com>
2019-08-19 20:05:33 +0100

    Moved sample scripts to separate folder

---
+++ sample_scripts/src/SampleScript_VectorDbl.cpp
@@ -0,0 +1,33 @@
+#include <toolbox/Algebra/Vectors.hpp>
+
+#include <iostream>
+
+using namespace Toolbox;
+
+
+int main()
+{
+    //const size_t N = 10;
+    //DynamicVector<double> a(N, 4), b(N, 6), c(N);
+    //a[3] = 2;
+    //auto result = a + b;
+    //c = std::move(result);
+    //std::cout << c << std::endl;
+
+    //DynamicVector<double> v1{ 1,5,2,9 };
+    //DynamicVector<double> v2{ 8,7,1,5 };
+    //
+    //StaticVector<double, 5> staticVec1{ 3,4,2,9,4 };
+    //StaticVector<double, 5> staticVec2(staticVec1);
+    //
+    //auto res = 3 * v1 + (v2 / 3 - 5 / v1) * v2;
+    //
+    //v1 - v2;
+    //
+    //std::string reStr = res.toString();
+    //v1 = res;
+    //
+    //std::cout << v1 << "\n";
+
+    return 0;
+}
