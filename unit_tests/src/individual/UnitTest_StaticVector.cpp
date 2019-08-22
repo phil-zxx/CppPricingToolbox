2c6902099af3a137abdc0ec0ded76013574dc231
phil-zxx <32247590+phil-zxx@users.noreply.github.com>
2019-08-22 20:14:18 +0100

    Added equal sign = in Unit Test for Static Vector

--- unit_tests/src/individual/UnitTest_StaticVector.cpp
+++ unit_tests/src/individual/UnitTest_StaticVector.cpp
@@ -20,17 +20,17 @@ TEST_CASE("UnitTest_StaticVector", "[UnitTest_StaticVector]")
     CHECK(vec1[3] == 6);
     CHECK(vec1[4] == 9);
 
-    const VectorType expr1(vec1 + vec2);
+    const VectorType expr1 = vec1 + vec2;
     const VectorType vecTrue1{ {12, 5, 11, 11, 10} };
     CHECK(expr1.size() == 5);
     CHECK(expr1 == vecTrue1);
 
-    const VectorType expr2(4 * ((-vec1 + vec2 / 8.) * 10 - vec1));
+    const VectorType expr2 = 4 * ((-vec1 + vec2 / 8.) * 10 - vec1);
     const VectorType vecTrue2{ { -185, -122, -43, -239, -391 } };
     CHECK(expr2.size() == 5);
     CHECK(expr2 == vecTrue2);
 
-    const VectorType expr3((-vec1 * vec2 / 2. + (vec1 - 5.) / vec2 - vec1) * 30 + 600);
+    const VectorType expr3 = (-vec1 * vec2 / 2. + (vec1 - 5.) / vec2 - vec1) * 30 + 600;
     const VectorType vecTrue3{ { -75,   390, 260,  -24,  315 } };
     CHECK(expr3.size() == 5);
     CHECK(expr3 == vecTrue3);
