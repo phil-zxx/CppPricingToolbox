e25533719488c27c70746e1a2ea32333927ddce7
phil-zxx <32247590+phil-zxx@users.noreply.github.com>
2020-05-18 13:12:46 +0100

    Enhanced UnitTest_Vector_BaseFunctions

--- unit_tests/src/individual/UnitTest_Algebra_Vector.cpp
+++ unit_tests/src/individual/UnitTest_Algebra_Vector.cpp
@@ -6,15 +6,23 @@ using namespace Toolbox;
 
 TEST_CASE("UnitTest_Vector_BaseFunctions")
 {
-    const StaticVector<double, 10> vecS(3);
+    const StaticVector<double, 10> vecS1(3), vecS2(3, 7);
     const DynamicVector<int> vecD(3, 5);
 
-    CHECK(vecS.capacity() == 10);
-    CHECK(vecS.size()     == 3);
-    CHECK(vecS[0]         == 0.);
-    CHECK(vecS[1]         == 0.);
-    CHECK(vecS.at(2)      == 0.);
-    CHECK_THROWS(vecS.at(3));
+    CHECK(vecS1.capacity() == 10);
+    CHECK(vecS1.size()     == 3);
+    CHECK(vecS1.capacity() == vecS2.capacity());
+    CHECK(vecS1.size()     == vecS2.size());
+    CHECK(vecS1[0]         == 0.);
+    CHECK(vecS1[1]         == 0.);
+    CHECK(vecS1[2]         == 0.);
+    CHECK(vecS2[0]         == 7.);
+    CHECK(vecS2[1]         == 7.);
+    CHECK(vecS2[2]         == 7.);
+    CHECK(vecS1.at(2)      == 0.);
+    CHECK(vecS2.at(2)      == 7.);
+    CHECK_THROWS(vecS1.at(3));
+    CHECK_THROWS(vecS2.at(3));
 
     CHECK(vecD.capacity() == 3);
     CHECK(vecD.size()     == 3);
@@ -151,16 +159,16 @@ TEST_CASE("UnitTest_Vector_Expressions")
         CHECK(prodResult2 == -315);
         CHECK(sumResult2  == 9);
     }
-    
+
     SUBCASE("Test_Expression1")
     {
         const auto expr1 = -(3 * vecS + 8 * asType<int>(vecD) - 3) / 2;
         const auto expr2 = trans(expr1);
         const DynamicVector<int, true> expr3(expr2);
 
-        CHECK(expr1[0] == 6);   CHECK(expr2[0] == 6);   CHECK(expr3[0] == 6);
+        CHECK(expr1[0] ==   6); CHECK(expr2[0] ==   6); CHECK(expr3[0] ==   6);
         CHECK(expr1[1] == -37); CHECK(expr2[1] == -37); CHECK(expr3[1] == -37);
-        CHECK(expr1[2] == -5);  CHECK(expr2[2] == -5);  CHECK(expr3[2] == -5);
+        CHECK(expr1[2] ==  -5); CHECK(expr2[2] ==  -5); CHECK(expr3[2] ==  -5);
         
         CHECK(expr1.transposeFlag == false);
         CHECK(expr2.transposeFlag == true);
