f49611a90bcfae28f29ccd2a448c8ea2eca78956
phil-zxx <32247590+phil-zxx@users.noreply.github.com>
2020-07-10 11:25:11 +0100

    Improved operator<< for Matrix: NaN displayed as "NAN" + Small values close to zero displayed as "." + Set width & precision per element

--- include/toolbox/Algebra/Types/Matrix.hpp
+++ include/toolbox/Algebra/Types/Matrix.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <sstream>
+#include <iomanip>
 
 
 namespace Toolbox
@@ -27,9 +28,17 @@ namespace Toolbox
             os << "[";
             for (size_t iRow = 0, rowCount = (~rhs).rowCount(); iRow < rowCount; ++iRow)
             {
-                os << "[ ";
+                os << (iRow == 0 ? "[" : " [");
                 for (size_t iCol = 0, colCount = (~rhs).colCount(); iCol < colCount; ++iCol)
-                    os << (~rhs)(iRow, iCol) << " ";
+                {
+                    const double val = (~rhs)(iRow, iCol);
+                    if (std::isnan(val))
+                        os << std::setw(10) << "NaN ";
+                    else if (std::abs(val) < 1.e-8)
+                        os << std::setw(10) << ". ";
+                    else
+                        os << std::setw(10) << std::setprecision(4) << val << " ";
+                }
                 os << "]" << (iRow + 1 < rowCount ? ",\n" : "");
             }
             return os << "]";
