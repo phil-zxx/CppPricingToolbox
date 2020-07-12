069297b115c3e96e68708eaa1eb899b4962acb89
phil-zxx <32247590+phil-zxx@users.noreply.github.com>
2020-07-12 01:17:06 +0100

    Updated code coverage script

--- code_coverage.sh
+++ code_coverage.sh
@@ -13,16 +13,16 @@ echo ">>> Running CMake"
 cmake -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage" -DCMAKE_BUILD_TYPE=Debug ..
 
 echo ">>> Running Make"
-make -j 8
+make -j 12
 
 echo ">>> Running Unit Tests"
-./../build/Debug_x86_64/UnitTests
+./../build/Debug_x86_64/UnitTests -tc=*Algebra*
 
 echo ">>> Running Code Coverage"
-lcov -d . -c -o coverage.info                                      # parse coverage data
-lcov -r coverage.info "/usr*"        -o coverage.info
-lcov -r coverage.info "*/external/*" -o coverage.info
-genhtml coverage.info -o out
+lcov -d . -c -o coverage.info --rc lcov_branch_coverage=1
+lcov -r coverage.info "/usr*"        -o coverage.info --rc lcov_branch_coverage=1
+lcov -r coverage.info "*/external/*" -o coverage.info --rc lcov_branch_coverage=1
+genhtml coverage.info -o out --legend --rc genhtml_branch_coverage=1
 
 echo ">>> Opening Code Coverage HTML"
 cd out
