795a337326776308f470cabd4ef76e240593b339
phil-zxx <32247590+phil-zxx@users.noreply.github.com>
2020-09-06 11:21:25 +0100

    Added Zip class (similar to Python zip function) + Unit Test

---
+++ include/toolbox/Generic/Zip.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <tuple>
+
+
+namespace Toolbox
+{
+    template<bool WithIndexFlag = false, class... Ts>
+    class Zip
+    {
+    public:
+        explicit Zip(const std::tuple<const Ts&...>& data)
+            : m_data(data) { }
+
+        explicit Zip(const Ts&... objs)
+            : m_data(objs...)
+        {
+            auto ensureSameSize = [N = std::get<0>(m_data).size()](const auto& obj) {
+                if (N != obj.size())
+                    throw std::out_of_range("Zip input vectors have non-matching sizes"); };
+            (ensureSameSize(objs), ...);
+        }
+
+        struct ZipIterator
+        {
+        public:
+            explicit ZipIterator(const std::tuple<const Ts&...>& data, size_t idx)
+                : m_data(data), m_idx(idx) { }
+
+            ZipIterator& operator++()
+            {
+                ++m_idx;
+                return *this;
+            }
+
+            ZipIterator operator++(int)
+            {
+                return ZipIterator(m_data, m_idx++);
+            }
+
+            bool operator!=(const ZipIterator& rhs) const
+            {
+                return m_idx != rhs.m_idx;
+            }
+
+            decltype(auto) operator*() const
+            {
+                if constexpr (WithIndexFlag)
+                    return std::apply([this](const auto&... obj) { return std::forward_as_tuple(m_idx, obj.at(m_idx)...); }, m_data);
+                else
+                    return std::apply([this](const auto&... obj) { return std::forward_as_tuple(obj.at(m_idx)...); }, m_data);
+            }
+
+        private:
+            const std::tuple<const Ts&...>& m_data;
+            size_t m_idx;
+        };
+
+        ZipIterator begin() const
+        {
+            return ZipIterator(m_data, 0);
+        }
+
+        ZipIterator end() const
+        {
+            return ZipIterator(m_data, std::get<0>(m_data).size());
+        }
+
+        decltype(auto) withIndex() const
+        {
+            return Zip<true, Ts...>(m_data);
+        }
+
+    private:
+        const std::tuple<const Ts&...> m_data;
+    };
+}
---
+++ unit_tests/src/Other/UnitTest_Zip.cpp
@@ -0,0 +1,46 @@
+#include <doctest/doctest.h>
+#include <toolbox/Generic/Zip.hpp>
+#include <sstream>
+#include <vector>
+#include <array>
+
+using namespace Toolbox;
+
+struct Dummy
+{
+    explicit Dummy(int value) noexcept
+        : m_value(value) { }
+
+    // This ensures that the Zip function below indeed makes no copies of the Dummy objects
+    Dummy(const Dummy& rhs) noexcept = delete;
+    Dummy(Dummy&& rhs) noexcept = delete;
+    Dummy& operator=(const Dummy& rhs) = delete;
+
+    friend std::ostream& operator<<(std::ostream& os, const Dummy& rhs)
+    {
+        return os << "Dummy(" << rhs.m_value << ")";
+    }
+
+private:
+    int m_value;
+};
+
+TEST_CASE("UnitTest_Zip")
+{
+    const std::vector<int> vec1{10, 20, 30};
+    std::array<Dummy, 3>   vec2{Dummy(0), Dummy(123), Dummy(-80)};
+    std::array<Dummy, 2>   vec3{Dummy(1), Dummy(456)};
+
+    std::ostringstream os1;
+    for (const auto& [a, b] : Zip(vec1, vec2))
+        os1 << a << "|" << b << " ";
+    CHECK(os1.str() == "10|Dummy(0) 20|Dummy(123) 30|Dummy(-80) ");
+
+    std::ostringstream os2;
+    for (const auto& [i, a, b] : Zip(vec1, vec2).withIndex())
+        os2 << i << ":" << a << "|" << b << " ";
+    CHECK(os2.str() == "0:10|Dummy(0) 1:20|Dummy(123) 2:30|Dummy(-80) ");
+
+    CHECK_NOTHROW(Zip(vec1, vec2, vec2));
+    CHECK_THROWS (Zip(vec3, vec2));
+}
